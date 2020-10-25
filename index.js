5fb0a3a9ba7e435bfaf4bbc6b665844a95f9200a
Tim Blanchard <timothy.blanchard@tesco.com>
2020-10-25 16:34:32 +0000

    2 passing tests

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 export const queueTime = (q, tillNum) => {
-    if(tillNum === 1){
-        return q.reduce((a,b) => a+b)
+    if (tillNum === 1) {
+        return q.reduce((a, b) => a + b)
     }
     const tills = [];
-    for(let i = 0; i<tillNum; i++){
+    for (let i = 0; i < tillNum; i++) {
         tills.push([])
     }
-    let runningTotalPerTill, nextAvailableTill;  
+    let runningTotalPerTill, nextAvailableTill;
     q.forEach(t => {
-         runningTotalPerTill = tills.map(till => till.reduce((a,b) => a+b,0))
-          
-         nextAvailableTill = runningTotalPerTill.indexOf(Math.min(runningTotalPerTill))
-         console.log('nat', nextAvailableTill)
+        runningTotalPerTill = tills.map(till => till.reduce((a, b) => a + b, 0))
+        nextAvailableTill = runningTotalPerTill.indexOf(Math.min(...runningTotalPerTill))
         tills[nextAvailableTill].push(t)
-
     })
-    return Math.max(runningTotalPerTill)
+    return Math.max(...tills.map(till => till.reduce((a, b) => a + b)))
 }
