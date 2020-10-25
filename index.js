c77a4e6fed65a9c057ab530edc5692a684886312
Tim Blanchard <timothy.blanchard@tesco.com>
2020-10-25 17:05:40 +0000

    WIP

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 export const queueTime = (q, tillNum) => {
     if (tillNum === 1) return q.reduce((a, b) => a + b,0)
-    const tills = [];
+//    let tills = new Array(tillNum).fill([])
+   let tills = [] // comment out until line 7
     for (let i = 0; i < tillNum; i++) {
         tills.push([])
     }
+    console.log(tillNum, tills)
+
     let runningTotalPerTill, nextAvailableTill;
     q.forEach(t => {
         runningTotalPerTill = tills.map(till => till.reduce((a, b) => a + b, 0))
         nextAvailableTill = runningTotalPerTill.indexOf(Math.min(...runningTotalPerTill))
         tills[nextAvailableTill].push(t)
+        console.log(tills)
     })
     return Math.max(...tills.map(till => till.reduce((a, b) => a + b, 0)))
 }
--- index.test.js
+++ index.test.js
@@ -8,7 +8,7 @@ describe('queueTime', () => {
     it('should sum array if only 1 till', () => {
         expect(queueTime([5,3,4], 1)).toEqual(12)
     });
-    it('should return correct result for 2 tills', () => {
+    it.only('should return correct result for 2 tills', () => {
         expect(queueTime([10,2,3,3], 2)).toEqual(10)
    })
  
